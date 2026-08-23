3e2b2347314157f3f9c0ec733f3e6d96d7699e42
AsherHuangDong <46062302+AsherHuangDong@users.noreply.github.com>
2026-08-24 06:04:20 +0800

    feat: add feedback runtime container

---
+++ src/presentation/components/feedback/FeedbackContainer.tsx
@@ -0,0 +1,27 @@
+import { useFeedback } from '../../hooks/useFeedback';
+import { XpGainToast } from './XpGainToast';
+import { QuestCompleteModal } from './QuestCompleteModal';
+import { UnlockNotification } from './UnlockNotification';
+import { BossPhaseChange } from './BossPhaseChange';
+
+export function FeedbackContainer() {
+  const feedback = useFeedback();
+
+  return (
+    <>
+      {feedback.toastQueue.map((toast) => (
+        <XpGainToast key={toast.id} {...toast} />
+      ))}
+
+      {feedback.modal && (
+        <QuestCompleteModal {...feedback.modal} />
+      )}
+
+      {feedback.notification && (
+        <UnlockNotification message={feedback.notification} />
+      )}
+
+      <BossPhaseChange />
+    </>
+  );
+}
